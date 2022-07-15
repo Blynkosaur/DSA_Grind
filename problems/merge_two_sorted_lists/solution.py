736ae168afecd74cb11e59a60f565a2fb89b0a19
mini-bryanlin <bryanlin316@gmail.com>
2022-07-15 02:07:40 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

--- problems/merge_two_sorted_lists/solution.py
+++ problems/merge_two_sorted_lists/solution.py
@@ -5,42 +5,48 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]):
-        # hashmap = {}
-        # def algorithm(mylist):
-        #     if mylist.val not in hashmap:
-        #         hashmap[mylist.val] = 0
-        #     hashmap[mylist.val]+= 1
-        #     if mylist.next != None:
-        #         algorithm(mylist.next)
-        # algorithm(list1)
-        # algorithm(list2)
-        # final = ListNode()
-        # def next():
-        #     for i in hashmap:
-        #         toset = final
-        #         for j in range(hashmap[i]):
-        #             toset.val = j
-        #             toset = toset.next
-        # return final
-        tail = ListNode()
+#         temp = ListNode()
+#         dummy = temp
+#         dummy.next = list1
+#         print("temp1: ", temp)
+#         dummy = dummy.next
+#         print(dummy)
+#         print("temp2: ",temp)
+#         dummy.next = list2
+#         print(dummy)
+#         print(temp)
         
-        dummy= tail
+        
+        
+        dummy = ListNode()
+        tail = dummy
+        
         while list1 and list2:
             if list1.val < list2.val:
-                dummy.next= list1
+                tail.next= list1
                 list1 = list1.next
+                print("list1: ",list1)
+                print("first tail: ",tail)
                 
             else:
-                dummy.next = list2
+                tail.next = list2
                 list2 = list2.next
+                print("list2",list2)
+                print("first tail: ",tail)
+                
             
-            dummy = dummy.next
-            print(dummy)
+            tail = tail.next
+            print("dummy: ",dummy)
+            print("tail: ",tail)
+            
         if list1:
-            dummy.next = list1
+            tail.next = list1
+            print("list1: ",list1)
         elif list2:
-            dummy.next = list2
-        return tail.next
+            tail.next = list2
+            print("list2: ",list2)
+        return dummy.next
+        
                 
         
                 
