27a0410eb32f982c629f10c8f508a2102a011cb7
mini-bryanlin <bryanlin316@gmail.com>
2022-07-20 15:29:12 +0000

    Sync LeetCode submission - Pow(x, n) (python3)

---
+++ problems/pow(x,_n)/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x,n)
+#         power = float(1)
+#         if n<0:
+            
+#             n = n*-1
+#             for i in range(n):
+#                 power = power/x
+                
+#             return power
+#         elif n == 0:
+#             return power
+            
+#         elif n>0:
+            
+#             for i in range(n):
+#                 power *=x
+#             return power
+        
+        
