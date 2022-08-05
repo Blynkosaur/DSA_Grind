8afd0c17cbd8c7fa1bc142688403efb02c7c15f1
mini-bryanlin <bryanlin316@gmail.com>
2022-08-05 17:58:43 +0000

    Sync LeetCode submission - Divide Two Integers (python3)

---
+++ problems/divide_two_integers/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        
+        quotient = int(dividend/divisor)
+        if quotient > 2**31 -1:
+            return 2**31 - 1
+        elif quotient <-2**31:
+            return -2**31
+        return quotient
