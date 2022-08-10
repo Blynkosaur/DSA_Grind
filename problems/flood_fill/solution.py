6a4b8bd859675b010167ce896ac2941a8dfef7b5
mini-bryanlin <bryanlin316@gmail.com>
2022-08-10 18:57:05 +0000

    Sync LeetCode submission - Flood Fill (python3)

--- problems/flood_fill/solution.py
+++ problems/flood_fill/solution.py
@@ -1,35 +1,26 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        r=len(image)
-        c=len(image[0])
-        
-        visited = [[False for i in range(len(image[0]))] for j in range(len(image))]
-        
-        q= deque()
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int):
+        q = deque()
+        original = image[sr][sc]
         q.append((sr,sc))
-        # image[sr][sc]=color
-        startingColor=image[sr][sc]
-        while(q):
-            coord= q.popleft()
-            x= coord[0]
-            y=coord[1]
-            
-
-            
-            if x-1>=0 and image[x-1][y]==image[x][y] and not visited[x-1][y] :
-                # image[x-1][y] = color
+        visited = [(sr,sc)]
+        while len(q) != 0:
+            coord = q.popleft()
+            x = coord[0]
+            y = coord[1]
+            if x-1>=0 and (x-1,y) not in visited and image[x-1][y]== original:
                 q.append((x-1,y))
-            if x+1<r and image[x+1][y]==image[x][y] and not visited[x+1][y] :
-                # image[x+1][y] = color
+                visited.append((x-1,y))
+            if x+1 <len(image) and (x+1,y) not in visited and image[x+1][y]== original:
                 q.append((x+1,y))
-            if y-1>=0 and image[x][y-1]==image[x][y] and not visited[x][y-1]:
-                # image[x][y-1] = color
-                q.append((x,y-1))
-            if y+1<c and image[x][y+1]==image[x][y] and not visited[x][y+1]:
-                # image[x][y+1] = color
+                visited.append((x-1,y))
+            if y+1 <len(image[0]) and (x,y+1) not in visited and image[x][y+1]== original:
                 q.append((x,y+1))
-                
-            image[x][y]=color
-            visited[x][y]=True
-            
+                visited.append((x,y+1))
+            if y-1 >=0and (x,y-1) not in visited and image[x][y-1]== original:
+                q.append((x,y-1))
+                visited.append((x,y-1))
+            image[x][y]= color
         return image
+        
+        
