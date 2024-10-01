e72d1891b2a4886b94967968ba4811120f534525
mini-bryanlin <bryanlin316@gmail.com>
2024-10-01 02:25:40 +0000

    Sync LeetCode submission - Binary Tree Right Side View (python3)

---
+++ problems/binary_tree_right_side_view/solution.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def __init__(self):
+        self.output = []
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        self.helper(root, 0)
+        nodes = []
+        for el in self.output:
+            nodes.append(el[-1])
+        return nodes
+
+
+
+    def helper(self, root, depth):
+        if not root:
+            return root
+
+        if depth > (len(self.output) - 1):
+            self.output.append([root.val])
+        else:
+            self.output[depth].append(root.val)
+
+        self.helper(root.left, depth + 1)
+        self.helper(root.right, depth + 1)
