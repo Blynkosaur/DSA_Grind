9271b9ae570aedb4d7b4333ab496d5eec92c912b
mini-bryanlin <bryanlin316@gmail.com>
2024-10-05 22:12:47 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- problems/combination_sum/solution.py
+++ problems/combination_sum/solution.py
@@ -1,19 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        def helper(array,target):
-            copy = array
-            if target < 0:
-                return 
-            elif target == 0 and sorted(array) not in res:
-                res.append(sorted(array))
-            else:
+        stack = collections.deque([])
+        for i in range(len(candidates)):
+            stack.append(([candidates[i]],candidates[i]))
+        while stack:
+            array = stack.popleft()
+            if array[1] == target and sorted(array[0]) not in res:
+                res.append(sorted(array[0]))
+            elif array[1] < target:
                 for el in candidates:
-                    temp = array.copy()
+                    temp = array[0].copy()
                     temp.append(el)
-                    helper(temp,target-el)
-                    
-        helper([],target)
+                    stack.append((temp,array[1]+el))
+            
 
         return res
                 
