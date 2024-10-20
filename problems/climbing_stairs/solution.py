49d9d410fbcfc8f547c6d4ac7c1e30fa5013f65a
mini-bryanlin <bryanlin316@gmail.com>
2024-10-20 02:13:35 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -1,8 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1,1
-        for i in range(n-1):
-            temp = one
-            one = one +two
-            two = temp
-        return one
+        hashmap = {1:1,2:2}
+        if n == 1:
+            return 1
+        elif n ==2:
+            return 2
+        else: 
+            for i in range(3,n+1):
+                hashmap[i] = hashmap[i-1]+hashmap[i-2]
+
+        return hashmap[n]
