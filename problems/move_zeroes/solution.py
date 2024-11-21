75d211e3df4948dd64e55b969e782b09759d17a0
mini-bryanlin <bryanlin316@gmail.com>
2024-11-21 02:47:48 +0000

    Sync LeetCode submission - Move Zeroes (python3)

---
+++ problems/move_zeroes/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        c = nums.count(0)
+        for el in range(c):
+            nums.remove(0)
+            nums.append(0)
