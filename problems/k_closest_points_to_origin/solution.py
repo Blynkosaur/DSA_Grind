6e45ae160a646b8d83f7464562d9fe946b0d08c8
mini-bryanlin <bryanlin316@gmail.com>
2024-12-18 04:41:49 +0000

    Sync LeetCode submission - K Closest Points to Origin (python3)

--- problems/k_closest_points_to_origin/solution.py
+++ problems/k_closest_points_to_origin/solution.py
@@ -1,16 +1,12 @@
 class Solution:
-    def takeSecond(self,elem):
-        return elem[1]
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        toreturn = []
         distances = []
+        ret = []
         for i in points:
             toadd = (i[0]**2)+(i[1]**2)
-            distances.append((i,toadd))
-        distances.sort(key=self.takeSecond)
+            distances.append((toadd,i))
+        heapify(distances)
         for i in range(k):
-            toreturn.append(distances[i][0])
-        return toreturn
-            
-        
-        
+            distance, point = heappop(distances)
+            ret.append(point)
+        return ret
