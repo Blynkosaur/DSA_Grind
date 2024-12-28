3b12778ba96d36a4f332e0330ee4e56487a03ac2
mini-bryanlin <bryanlin316@gmail.com>
2024-12-28 18:34:56 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

---
+++ problems/merge_sorted_array/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        two = 0
+        for i in range(m,len(nums1)):
+            nums1[i] = nums2[two]
+            two += 1
+        nums1.sort()
+        return nums1
+        
