67586c95427d54936cfbfce582eb8a8aea447425
mini-bryanlin <bryanlin316@gmail.com>
2026-06-14 02:18:35 +0000

    Sync LeetCode submission - Product of Array Except Self (python3)

--- problems/product_of_array_except_self/solution.py
+++ problems/product_of_array_except_self/solution.py
@@ -1,39 +1,43 @@
-import math
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        out = []
-        left = [1]
-        right = [1]
+        #prefix products
+        left_prefix = []
+        right_prefix = []
+        final_products = []
+        """[1,2,3,4,5]
+        
+        n =5
+            [120,60,40,30,24]
+        left [1,2,6,24,120]
+        right [5,20,60,120,120]
+        
+        """
         left_prod = 1
         right_prod = 1
-        left_count = 1
-        right_count = -1
-        for i in range(len(nums)-1):
-            left_prod *= nums[left_count-1]
-            left.append(left_prod)
-            left_count += 1
-            right_prod *= nums[right_count]
-            right.append(right_prod)
-            right_count -= 1
-
-        print(left)
-        print(right)
-
-        
-        right.reverse()
-        # prod = 1
-        # for i in range(1,len(nums)):
-        #     prod *= nums[i-1]
-        #     right.append(prod)
-        # right.reverse()
-        
         for i in range(len(nums)):
-            out.append(left[i]*right[i])
-       
+            left_prod *= nums[i] 
+            left_prefix.append(left_prod)
+            right_prod *= nums[len(nums)-1-i]
+            right_prefix.append(right_prod)
+        print(left_prefix, right_prefix)
+
+        for i in range(len(nums)):
+            curr_prod = 1
+            left_index = i-1
+            right_index = len(nums)-i-2
+            print(f"left: {left_index} right:{right_index}")
+            curr_prod = curr_prod*left_prefix[left_index] if left_index >= 0 else curr_prod 
+            curr_prod = curr_prod*right_prefix[right_index] if right_index >= 0 else curr_prod 
+            final_products.append(curr_prod)
+        return final_products
+
+            
+
 
         
+        
+        
 
-           
-        return out
+
 
         
