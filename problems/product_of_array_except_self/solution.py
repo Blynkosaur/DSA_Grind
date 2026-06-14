a4705357e2f34e09f5285be61580bc141c01688d
mini-bryanlin <bryanlin316@gmail.com>
2026-06-14 02:40:26 +0000

    Sync LeetCode submission - Product of Array Except Self (python3)

--- problems/product_of_array_except_self/solution.py
+++ problems/product_of_array_except_self/solution.py
@@ -1,10 +1,12 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         #prefix products
-        left_prefix = []
-        right_prefix = []
-        final_products = []
+        # left_prefix = []
+        # right_prefix = []
+        final_products = [1 for i in range(len(nums))]
         """[1,2,3,4,5]
+            [1,1,2,6,24]
+            []
         
         n =5
             [120,60,40,30,24]
@@ -12,23 +14,33 @@ class Solution:
         right [5,20,60,120,120]
         
         """
-        left_prod = 1
-        right_prod = 1
-        for i in range(len(nums)):
-            left_prod *= nums[i] 
-            left_prefix.append(left_prod)
-            right_prod *= nums[len(nums)-1-i]
-            right_prefix.append(right_prod)
-        print(left_prefix, right_prefix)
+        curr_prod = 1
+        for i in range(len(nums)-1):
+            curr_prod*=nums[i]
+            final_products[i+1] *= curr_prod
 
-        for i in range(len(nums)):
-            curr_prod = 1
-            left_index = i-1
-            right_index = len(nums)-i-2
-            print(f"left: {left_index} right:{right_index}")
-            curr_prod = curr_prod*left_prefix[left_index] if left_index >= 0 else curr_prod 
-            curr_prod = curr_prod*right_prefix[right_index] if right_index >= 0 else curr_prod 
-            final_products.append(curr_prod)
+        curr_prod = 1
+        for i in range(len(nums)-1,0,-1):
+            curr_prod*=nums[i]
+            final_products[i-1] *= curr_prod
+             
+        # left_prod = 1
+        # right_prod = 1
+        # for i in range(len(nums)):
+        #     left_prod *= nums[i] 
+        #     left_prefix.append(left_prod)
+        #     right_prod *= nums[len(nums)-1-i]
+        #     right_prefix.append(right_prod)
+        # print(left_prefix, right_prefix)
+
+        # for i in range(len(nums)):
+        #     curr_prod = 1
+        #     left_index = i-1
+        #     right_index = len(nums)-i-2
+        #     print(f"left: {left_index} right:{right_index}")
+        #     curr_prod = curr_prod*left_prefix[left_index] if left_index >= 0 else curr_prod 
+        #     curr_prod = curr_prod*right_prefix[right_index] if right_index >= 0 else curr_prod 
+        #     final_products.append(curr_prod)
         return final_products
 
             
