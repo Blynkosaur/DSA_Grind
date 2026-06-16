c23fcfa0a280f58a52e94aaf2b8aacf985ae8d82
mini-bryanlin <bryanlin316@gmail.com>
2026-06-16 15:55:51 +0000

    Sync LeetCode submission - Majority Element (python3)

--- problems/majority_element/solution.py
+++ problems/majority_element/solution.py
@@ -1,10 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        majority = int(len(nums)/2)+1
-        hashmap = {}
-        for i in nums:
-            if i not in hashmap:
-                hashmap[i]=0
-            hashmap[i] += 1
-            if hashmap[i]== majority:
-                return i
+        res, count = 0,0
+        for num in nums:
+            if count == 0:
+                res = num
+            count = count+1 if num == res else count-1
+        return res
+
+            
+            
+        
+        
