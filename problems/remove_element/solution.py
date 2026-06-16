ccb4aef9d7d600f3f67eb6ee09d1dee2cb8f49de
mini-bryanlin <bryanlin316@gmail.com>
2026-06-16 15:03:02 +0000

    Sync LeetCode submission - Remove Element (python3)

--- problems/remove_element/solution.py
+++ problems/remove_element/solution.py
@@ -1,5 +1,21 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        while val in nums:
-            nums.remove(val)
-        return len(nums)
+        right = len(nums)-1
+        left = 0
+        count = 0
+        def move_right():
+            nonlocal right, count
+            while  right >=0 and nums[right] == val:
+                right -= 1
+                count += 1
+        move_right()
+        while left < right:
+            if nums[left] == val:
+                nums[left], nums[right] = nums[right], nums[left] 
+                move_right()
+            left += 1
+                
+        
+        return len(nums)-count
+            
+        
