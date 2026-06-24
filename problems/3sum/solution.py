50adf91faed1efd4b87a10f7ebdf77babd3ba04f
mini-bryanlin <bryanlin316@gmail.com>
2026-06-24 23:52:23 +0000

    Sync LeetCode submission - 3Sum (python3)

--- problems/3sum/solution.py
+++ problems/3sum/solution.py
@@ -1,23 +1,42 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
-        answer = []
-        for i,n in enumerate(nums):
-            if i > 0 and n == nums[i-1] :
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        outputs = set() 
+        nums.sort()
+
+        for i in range(len(nums)):
+            target = -1 * nums[i]
+            left = 0
+            right = len(nums)-1
+
+            if nums[left] + nums[left+1] > target:
                 continue
-            left,right = i+1, len(nums)-1
-            while left<right:
-                
-                threesum = n + nums[left] + nums[right]
-                
-                if threesum <0:
-                    left += 1
-                elif threesum >0:
-                    right -=1
-                elif threesum == 0:
-                    
-                    answer.append([n,nums[left],nums[right]])
-                    left += 1
-                    while nums[left] == nums[left-1] and left<right:
+
+            elif nums[right] + nums[right-1] < target:
+                continue
+
+            else:
+                while left < right:
+                    if left == i:
                         left += 1
-        return answer
+                        continue
+                    elif right == i:
+                        right -= 1
+                        continue
+
+                    elif nums[left] + nums[right] == target:
+                        outputs.add(tuple(sorted([nums[i],nums[left],nums[right]])))
+                        left += 1
+                        while left < right and nums[left] == nums[left-1]:
+                            left += 1
+                        right -= 1
+                        while left<right and nums[right] == nums[right+1]:
+                            right -= 1
+
+                    elif nums[left] + nums[right] < target:
+                        left += 1
+
+                    else:
+                        right -= 1
+
+        return [list(out) for out in outputs]
+
