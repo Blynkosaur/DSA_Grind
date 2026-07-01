89e343e80769d6f89d645435742497a0526cf67f
mini-bryanlin <bryanlin316@gmail.com>
2026-07-01 22:11:13 +0000

    Sync LeetCode submission - Permutation in String (python3)

--- problems/permutation_in_string/solution.py
+++ problems/permutation_in_string/solution.py
@@ -1,14 +1,29 @@
 class Solution:
+    def isValid(self, c1, c2) -> bool:
+        for c in c1:
+            if c1[c] != c2.get(c, 0):
+                return False
+        return True
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        counter = {}
+        sub_counter = {}
+        if len(s1) > len(s2):
+            return False
+        for c in s1:
+            sub_counter[c]= sub_counter.get(c,0) + 1
+        for i in range(len(s1)-1):
+            counter[s2[i]] = counter.get(s2[i], 0) + 1
         left = 0
-        right = len(s1)
-        ss1 = sorted(s1)
-        
-        for i in range(len(s2)-len(s1)+1):
-            ss2 = s2[left:right]
-            ss2 = sorted(ss2)
-            if ss1 == ss2:
+        right = len(s1) - 1 
+        for i in range(len(s2) - len(s1) + 1):
+            counter[s2[right]] = counter.get(s2[right],0) + 1
+            if self.isValid(sub_counter, counter):
                 return True
-            right += 1
+            counter[s2[left]] -= 1
             left += 1
+            right += 1
         return False
+            
+            
+            
+        
