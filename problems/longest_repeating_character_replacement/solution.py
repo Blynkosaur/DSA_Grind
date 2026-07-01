84f1ae472e4ff96cf92b4eebd84859b75478b30d
mini-bryanlin <bryanlin316@gmail.com>
2026-07-01 01:00:28 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (python3)

--- problems/longest_repeating_character_replacement/solution.py
+++ problems/longest_repeating_character_replacement/solution.py
@@ -1,27 +1,27 @@
 class Solution:
-    def characterReplacement(self,s: str, k: int) -> int:
-        left = 0 
-        right = 0
-        maximum = 0
-        hashmap = {s[left]:0}
-        popular =1
-        while right < len(s):
-            
-            
-                
-            if s[right] not in hashmap:
-                hashmap[s[right]] = 1
-            else:
-                hashmap[s[right]] += 1
-            popular  = max(hashmap.values())
-            if (right-left+1)- popular > k:
-                hashmap[s[left]] -= 1
-                left += 1
+    def characterReplacement(self, s: str, k: int) -> int:
+        longest = 1
+        curr = 0
+        maxf = 0
+        hashmap = {s[0]:1}
+        for i in range(1,len(s)):
+            hashmap[s[i]] = hashmap.get(s[i],0) + 1
+            maxf = max(maxf, hashmap[s[i]])
 
-            maximum = max(maximum, right-left+1)
+            while i - curr + 1 - maxf > k:
+                hashmap[s[curr]] -= 1
+                curr += 1
+            
+            longest = max(longest, i - curr + 1)
+        longest = max(longest, len(s) - curr)
+            
+            
+        return longest
+                    
+                
                 
             
-            right += 1
+            
+            
+
         
-        
-        return maximum
