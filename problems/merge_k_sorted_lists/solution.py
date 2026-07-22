ba002a63ef15666ae8f4ebe6aa720f7e354ea563
mini-bryanlin <bryanlin316@gmail.com>
2026-07-22 23:17:37 +0000

    Sync LeetCode submission - Merge k Sorted Lists (python3)

--- problems/merge_k_sorted_lists/solution.py
+++ problems/merge_k_sorted_lists/solution.py
@@ -4,25 +4,29 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def mergeTwoLists(self, one: ListNode, two:ListNode)->ListNode:
+        head = ListNode(0)
+        dummy = head
+        while one and two:
+            if one.val < two.val:
+                head.next = ListNode(one.val)
+                one = one.next
+            else:
+                head.next = ListNode(two.val)
+                two = two.next
+            head = head.next
+        if one:
+            head.next = one
+        if two:
+            head.next = two
+        return dummy.next
+
+
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
         while len(lists) > 1:
-            ret = ListNode(0)
-            head = ret
-            l1 = lists.pop()
-            l2 = lists.pop()
-            while l1 and l2:
-                if l1.val < l2.val:
-                    head.next = l1
-                    l1 = l1.next
-                else:
-                    head.next = l2
-                    l2 = l2.next
-                head = head.next  
-
-            head.next = l1 if l1 else l2
-            lists.append(ret.next)
+            first = lists.pop()
+            second = lists.pop()
+            lists.append(self.mergeTwoLists(first,second))
         return lists[0]
-
-                
