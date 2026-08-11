79f0d778c6e78d56259b334e4283e4d6a2050f24
mini-bryanlin <bryanlin316@gmail.com>
2026-08-11 16:45:27 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- problems/combination_sum/solution.py
+++ problems/combination_sum/solution.py
@@ -1,20 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        stack = collections.deque([])
-        for i in range(len(candidates)):
-            stack.append(([candidates[i]],candidates[i]))
-        while stack:
-            array = stack.popleft()
-            if array[1] == target and sorted(array[0]) not in res:
-                res.append(sorted(array[0]))
-            elif array[1] < target:
-                for el in candidates:
-                    temp = array[0].copy()
-                    temp.append(el)
-                    stack.append((temp,array[1]+el))
-            
-
-        return res
-                
-                
+        all_cand = []
+        def backtrack(current_nums, total, last):
+            if total > target:
+                return
+            if total == target:
+                all_cand.append(current_nums)
+                return
+            for c in candidates:
+                if c >= last:
+                    backtrack(current_nums + [c], total + c, c)
+        backtrack([], 0, 0)
+        return all_cand
+        
