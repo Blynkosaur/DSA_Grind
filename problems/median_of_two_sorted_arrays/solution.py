481e39bdf83ec0c2da9f9b9ccc68f1277bd8be45
mini-bryanlin <bryanlin316@gmail.com>
2026-08-14 04:41:36 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (python3)

--- problems/median_of_two_sorted_arrays/solution.py
+++ problems/median_of_two_sorted_arrays/solution.py
@@ -1,9 +1,45 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        ls = sorted(nums1 + nums2)
+        #check for overlap
+        A, B = nums1, nums2
+        if len(B) < len(A):
+            A,B = B,A
+        total_len = len(A) + len(B)
+        half = total_len//2
+        left = 0
+        right = len(A) -1 
+        while True:
+            mid1 = (left+right)//2
+            mid2 = half - mid1 - 2
+            A_left = float('-inf') if mid1 < 0 else A[mid1]
+            A_right = float('inf') if mid1 + 1 >= len(A) else A[mid1 + 1]
+            B_left = float('-inf') if mid2 < 0 else B[mid2] 
+            B_right = float('inf') if mid2 + 1 >= len(B) else B[mid2 + 1]
+
+            #correct partition 
+            if A_left <= B_right and B_left <= A_right:
+                if total_len % 2: 
+                    return min(A_right, B_right)
+                else:
+                    return (max(A_left, B_left) + min(A_right, B_right))/2
+
+            elif A_left > B_right:
+                right = mid1 -1 
+            else:
+                left = mid1 + 1
+
         
-        if len(ls)%2 == 1:
-            return ls[len(ls)//2]
-        else:
-            return (ls[len(ls)//2] +ls[(len(ls)//2)-1])/2
+            
+            
+        
+        
+
+
+            
+
+
+
+
+        
+        
         
