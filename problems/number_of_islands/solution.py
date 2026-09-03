8252cadf69de6e72d5ea32ec0d66ed23e9372063
mini-bryanlin <bryanlin316@gmail.com>
2026-09-03 01:18:51 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -1,28 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-            count = 0
-            visited= [[False for i in range(len(grid[0]))] for j in range(len(grid))]
-            for i in range(len(grid)):
-                for j in range(len(grid[0])):
-                    if grid[i][j] == "1" and visited[i][j] == False:
-                        q = deque()
-                        q.append((i,j))
-                        visited[i][j] = True
-                        while len(q) != 0:
-                            coord = q.popleft()
-                            x = coord[0]
-                            y = coord[1]
-                            if x-1 >= 0 and grid[x-1][y] == "1" and visited[x-1][y] == False:
-                                q.append((x-1,y))
-                                visited[x-1][y]= True
-                            if x+1 < len(grid)  and grid[x+1][y] == "1" and visited[x+1][y] == False:
-                                q.append((x+1,y))
-                                visited[x+1][y]= True
-                            if y-1 >= 0 and grid[x][y-1] == "1" and visited[x][y-1] == False:
-                                q.append((x,y-1))
-                                visited[x][y-1]= True
-                            if y+1 < len(grid[0]) and grid[x][y+1] == "1" and visited[x][y+1] == False:
-                                q.append((x,y+1))
-                                visited[x][y+1]= True
-                        count +=1
-            return count
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+        def bfs(r, c):
+            q = deque([(r,c)])
+            dirs = [(-1,0), (1,0), (0,-1), (0, 1)]
+            while q:
+                r,c = q.popleft()
+                visited.add((r,c))
+                for dx, dy in dirs:
+                    px, py = r + dx, c + dy
+                    if px in range(rows) and py in range(cols) and (px, py) not in visited and grid[px][py] == "1":
+                        q.append((px,py))
+                        visited.add((px,py))
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c)
+                    islands += 1
+        return islands
+
+
+
+        
